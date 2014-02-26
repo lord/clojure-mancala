384b18922239223d125b265b76cb0f31251919d2
Robert Lord <robert@lord.io>
2014-02-26 14:10:25 -0500

    Add game over checking function that also calculates final scores

--- src/genetic_mancala/core.clj
+++ src/genetic_mancala/core.clj
@@ -29,4 +29,25 @@
              (mod (+ turn 1) 2))}))
 
 
-(make-move {:board [3 3 3 3 3 3 3 3 3 3 3 3 3 3 3] :turn 0} 3 0)
+(make-move {:board [3 3 3 12 3 3 3 3 3 3 3 3 3 3 3] :turn 0} 3 0)
+
+(defn squares-empty?
+  "Checks if all the elements in squares are equal to 0"
+  [squares]
+  (if (< 0 (reduce #(if (> %1 %2) %1 %2) squares))
+    false
+    true))
+
+(squares-empty? [0 0 0 0 1 0])
+
+(defn final-scores
+  "Checks if a game is over, returns final scores if it is, otherwise false"
+  [state]
+  (let [board (:board state)
+        p0-houses (take 6 board)
+        p1-houses (subvec board 7 13)]
+    (if (or (squares-empty? p0-houses)
+            (squares-empty? p1-houses))
+      [(+ (nth board 06) (reduce + p0-houses))
+       (+ (nth board 13) (reduce + p1-houses))]
+      false)))
